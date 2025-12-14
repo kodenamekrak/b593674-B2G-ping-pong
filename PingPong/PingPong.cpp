47fb170aacd3ace3b520ef7d33047c104d3cbfc5
kodenamekrak <kyelodge2005@proton.me>
2025-12-14 15:32:22 +0000

    Add additional inputs

--- PingPong/PingPong.cpp
+++ PingPong/PingPong.cpp
@@ -123,6 +123,9 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 						process_button(BUTTON_DOWN, VK_DOWN);
 						process_button(BUTTON_W, 'W');
 						process_button(BUTTON_S, 'S');
+						process_button(BUTTON_LEFT, VK_LEFT);
+						process_button(BUTTON_RIGHT, VK_RIGHT);
+						process_button(BUTTON_ENTER, VK_RETURN);
 
 						case VK_ESCAPE:
 						{
